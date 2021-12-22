27623ee4e69eb34e23a511c639b43a89310cecd0
gpatankar21 <gpatankar21@gmail.com>
2021-12-22 23:18:50 +0530

    Added changes for javascript frontend

--- src/main/java/com/myprojects/wordsearch/WordSearchController.java
+++ src/main/java/com/myprojects/wordsearch/WordSearchController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,8 @@ public class WordSearchController {
 	WordSearchService service;
 	
 
-	@GetMapping("/wordsearch")	
+	@GetMapping("/wordsearch")
+	@CrossOrigin
 	public String getGrid(@RequestParam int gridSize, @RequestParam String words)
 	{
 		List<String>list_of_words = Arrays.asList(words.split(","));
